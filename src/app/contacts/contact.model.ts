6bc0baf0a165f70e653425beb7e80d8dba91f76e
allgiff <gif19002@byui.edu>
2022-09-22 18:01:44 -0600

    lesson 2

--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -4,7 +4,7 @@ export class Contact {
         public name: string,
         public email: string,
         public phone: string,
-        public imageurl: string,
+        public imageUrl: string,
         public group: Contact[]
     ){}
 }
