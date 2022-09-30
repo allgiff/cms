726489c55d2ea6ae6209afe9d476ba955fc1561c
allgiff <gif19002@byui.edu>
2022-09-29 18:08:32 -0600

    lesson 3 message and header components

---
+++ src/app/messages/message-list/message-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
+
+@Component({
+  selector: 'cms-message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['./message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message('1', 'Subject 1', 'Message Text 1', 'Allie'),
+    new Message('2', 'Subject 2', 'Message Text 2', 'Allie'),
+    new Message('3', 'Subject 3', 'Message Text 3', 'Allie')
+  ];
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
+
+}
